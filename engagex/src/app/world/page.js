622645203edf6531c1804547637fb7b6b7aa1234
kanhaiya86agency <kanhaiya.86agency@gmail.com>
2025-01-03 14:27:28 +0530

    fix: implement drawer

---
+++ engagex/src/app/world/page.js
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+import GlobeLayout from "./Earth";
+
+const globel = () => {
+  return (
+    <div>
+      <Navbar />
+      <GlobeLayout />
+    </div>
+  );
+};
+
+export default globel;

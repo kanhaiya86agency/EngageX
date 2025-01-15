bf553afbd01ecf20d4d9f3d054805247e5db4a5b
prathamesh86 <prathamesh.86agency@gmail.com>
2025-01-15 16:24:48 +0530

    fix : updated build

--- engagex/public/UserData.js
+++ engagex/public/UserData.js
@@ -53,112 +53,112 @@ export const userData = [
     profilePicture:
       "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User5.svg",
   },
-  {
-    id: 7,
-    name: "Ivan Davis",
-    latitude: -1.2921,
-    longitude: 36.8219, // Nairobi, Africa
-    continent: "Africa",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User4.svg",
-  },
-  {
-    id: 8,
-    name: "Liam Lewis",
-    latitude: -34.6037,
-    longitude: -58.3816, // Buenos Aires, South America
-    continent: "South America",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User2.svg",
-  },
-  {
-    id: 9,
-    name: "Mia Robinson",
-    latitude: 35.6762,
-    longitude: 139.6503, // Tokyo, Asia
-    continent: "Asia",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User5.svg",
-  },
-  {
-    id: 10,
-    name: "Noah Hall",
-    latitude: -26.2041,
-    longitude: 28.0473, // Johannesburg, Africa
-    continent: "Africa",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User8.svg",
-  },
-  {
-    id: 11,
-    name: "Olivia Martinez",
-    latitude: 19.4326,
-    longitude: -99.1332, // Mexico City, North America
-    continent: "North America",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User2.svg",
-  },
-  {
-    id: 12,
-    name: "Paul Walker",
-    latitude: 51.5074,
-    longitude: -0.1278, // London, Europe
-    continent: "Europe",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User7.svg",
-  },
-  {
-    id: 13,
-    name: "Quinn Carter",
-    latitude: 28.6139,
-    longitude: 77.209,
-    continent: "Asia", // Delhi, Asia
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User9.svg",
-  },
-  {
-    id: 14,
-    name: "Rebecca Adams",
-    latitude: -41.2865,
-    longitude: 174.7762, // Wellington, Australia
-    continent: "Australia",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User10.svg",
-  },
-  {
-    id: 15,
-    name: "Samuel Wilson",
-    latitude: 40.7128,
-    longitude: -74.006, // New York, North America
-    continent: "North America",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User11.svg",
-  },
-  {
-    id: 16,
-    name: "Tina Garcia",
-    latitude: -12.0464,
-    longitude: -77.0428, // Lima, South America
-    continent: "South America",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User12.svg",
-  },
-  {
-    id: 17,
-    name: "Uma Nelson",
-    latitude: 6.9271,
-    longitude: 79.8612, // Colombo, Asia
-    continent: "Asia",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User13.svg",
-  },
-  {
-    id: 18,
-    name: "Victor Green",
-    latitude: -17.8249,
-    longitude: 31.053, // Harare, Africa
-    continent: "Africa",
-    profilePicture:
-      "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User14.svg",
-  },
+  // {
+  //   id: 7,
+  //   name: "Ivan Davis",
+  //   latitude: -1.2921,
+  //   longitude: 36.8219, // Nairobi, Africa
+  //   continent: "Africa",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User4.svg",
+  // },
+  // {
+  //   id: 8,
+  //   name: "Liam Lewis",
+  //   latitude: -34.6037,
+  //   longitude: -58.3816, // Buenos Aires, South America
+  //   continent: "South America",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User2.svg",
+  // },
+  // {
+  //   id: 9,
+  //   name: "Mia Robinson",
+  //   latitude: 35.6762,
+  //   longitude: 139.6503, // Tokyo, Asia
+  //   continent: "Asia",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User5.svg",
+  // },
+  // {
+  //   id: 10,
+  //   name: "Noah Hall",
+  //   latitude: -26.2041,
+  //   longitude: 28.0473, // Johannesburg, Africa
+  //   continent: "Africa",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User8.svg",
+  // },
+  // {
+  //   id: 11,
+  //   name: "Olivia Martinez",
+  //   latitude: 19.4326,
+  //   longitude: -99.1332, // Mexico City, North America
+  //   continent: "North America",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User2.svg",
+  // },
+  // {
+  //   id: 12,
+  //   name: "Paul Walker",
+  //   latitude: 51.5074,
+  //   longitude: -0.1278, // London, Europe
+  //   continent: "Europe",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User7.svg",
+  // },
+  // {
+  //   id: 13,
+  //   name: "Quinn Carter",
+  //   latitude: 28.6139,
+  //   longitude: 77.209,
+  //   continent: "Asia", // Delhi, Asia
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User9.svg",
+  // },
+  // {
+  //   id: 14,
+  //   name: "Rebecca Adams",
+  //   latitude: -41.2865,
+  //   longitude: 174.7762, // Wellington, Australia
+  //   continent: "Australia",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User10.svg",
+  // },
+  // {
+  //   id: 15,
+  //   name: "Samuel Wilson",
+  //   latitude: 40.7128,
+  //   longitude: -74.006, // New York, North America
+  //   continent: "North America",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User11.svg",
+  // },
+  // {
+  //   id: 16,
+  //   name: "Tina Garcia",
+  //   latitude: -12.0464,
+  //   longitude: -77.0428, // Lima, South America
+  //   continent: "South America",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User12.svg",
+  // },
+  // {
+  //   id: 17,
+  //   name: "Uma Nelson",
+  //   latitude: 6.9271,
+  //   longitude: 79.8612, // Colombo, Asia
+  //   continent: "Asia",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User13.svg",
+  // },
+  // {
+  //   id: 18,
+  //   name: "Victor Green",
+  //   latitude: -17.8249,
+  //   longitude: 31.053, // Harare, Africa
+  //   continent: "Africa",
+  //   profilePicture:
+  //     "https://hrmportal.s3.ap-south-1.amazonaws.com/userDataImages/User14.svg",
+  // },
 ];
--- engagex/src/app/world/MarkerPin.js
+++ engagex/src/app/world/MarkerPin.js
@@ -2,12 +2,11 @@ import React from "react";
 
 const MarkerPin = ({ visibleProfile }) => {
   return (
-    // <img
-    //   className="absolute top-[-10px] lg:top-[-28px] left-[16px] lg:left-[14px] lg:w-[60px] w-[50px]  rounded-full opacity-100 text-sky-50 p-1"
-    //   src={visibleProfile?.profilePicture}
-    //   alt={visibleProfile?.name}
-    // />
-    <></>
+    <img
+      className="absolute top-[-10px] lg:top-[-28px] left-[16px] lg:left-[14px] lg:w-[60px] w-[50px]  rounded-full opacity-100 text-sky-50 p-1"
+      src={visibleProfile?.profilePicture}
+      alt={visibleProfile?.name}
+    />
   );
 };
 
